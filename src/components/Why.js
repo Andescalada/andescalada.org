84226b37e170c6fec6c1d4a21b97ee2be55fb505
daquiroz <daquiroz@uc.cl>
2020-11-23 18:51:04 -0300

    iconos added to why

--- src/components/Why.js
+++ src/components/Why.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import icono_cuidado from '../img/icono_cuidado.svg'
+import icono_acceso from '../img/icono_acceso.svg'
+import icono_desarrollo from '../img/icono_desarrollo.svg'
+import icono_documentacion from '../img/icono_documentacion.svg'
 
 const Why = class extends React.Component {
   render() {
@@ -13,10 +17,26 @@ const Why = class extends React.Component {
               Soñamos con comunidades que velen por el cuidado, acceso, desarrollo y documentación de los sectores de escalada
             </h3>
             <div className="columns is-hidden-mobile">
-              <div className="column">La protección del medio ambiente es clave para preservar los espacios de escalada y potenciar nuevo lugares.</div>
-              <div className="column">Aspiramos que todos puedan aprovechar de forma respetuosa la naturaleza, en particular los sectores de escalada.</div>
-              <div className="column">Al potenciar los sectores de escalada, se potencia todo un ecosistema en torno a ellos que permite desarrollar comunidades locales.</div>
-              <div className="column">Con información actualizada y centralizada existirán menos problemas en los acuerdos y mayor seguridad.</div>
+              <div className="column">
+                <img src={icono_cuidado} alt="Cuidado" />
+                <h4 className="title is-size-5">Cuidado</h4>
+                <p>La protección del medio ambiente es clave para preservar los espacios de escalada y potenciar nuevo lugares.</p>
+              </div>
+              <div className="column">
+                <img src={icono_acceso} alt="Acceso" />
+                <h4 className="title is-size-5">Acceso</h4>
+                <p>Aspiramos que todos puedan aprovechar de forma respetuosa la naturaleza, en particular los sectores de escalada.</p>
+              </div>
+              <div className="column">
+                <img src={icono_desarrollo} alt="Desarrollo" />
+                <h4 className="title is-size-5">Desarrolo</h4>
+                <p>Al potenciar los sectores de escalada, se potencia todo un ecosistema en torno a ellos que permite desarrollar comunidades locales.</p>
+              </div>
+              <div className="column">
+                <img src={icono_documentacion} alt="Documentación" />
+                <h4 className="title is-size-5">Documentación</h4>
+                <p>Con información actualizada y centralizada existirán menos problemas en los acuerdos y mayor seguridad.</p>
+              </div>
             </div>
           </div>
         </div>
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -59,36 +59,12 @@ export const IndexPageTemplate = ({
     <Challenges />
     <Team />
     <Contact />
-    <section className="section section--gradient">
+    <section className="section is-light">
       <div className="container">
         <div className="section">
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest stories
