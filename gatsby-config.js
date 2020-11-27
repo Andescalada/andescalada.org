deca95dced9183ce8aeed10ba009ab3567625f54
daquiroz <daquiroz@uc.cl>
2020-11-27 00:33:02 -0300

    title and favicon updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Andescalada',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Fundación que busca coordinar, documentar y promover el desarrollo sostenible de los sectores de escalada en torno a los Andes',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,7 +19,7 @@ const TemplateWrapper = ({ children }) => {
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -28,13 +28,13 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          href={`${withPrefix('/')}img/favicon_32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          href={`${withPrefix('/')}img/favicon_16.png`}
           sizes="16x16"
         />
 
