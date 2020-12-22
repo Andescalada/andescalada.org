b40bb7e199d752a5083579469ac7573643693762
daquiroz <daquiroz@uc.cl>
2020-12-22 15:42:43 -0300

    brandig updated

--- src/templates/brand-page.js
+++ src/templates/brand-page.js
@@ -40,6 +40,7 @@ export const BrandPageTemplate = ({
                 </h3>
                 <div className="pt-3 pb-6">
                   <img className=" is-spaced" src={logo} alt="logo Andescalada" style={{ height: '200px' }} />
+                  <br></br><a href="https://andescalada.org/img/logo_svg.svg" target="_blank">SVG </a> | <a href="https://andescalada.org/img/logo_morado.png" target="_blank">PNG </a>
                 </div>
               </section>
 
@@ -52,24 +53,29 @@ export const BrandPageTemplate = ({
                     <div className="column">
                       <h4 className="title is-size-5 is-size-5-mobile is-spaced"> Version negativo </h4>
                       <img className=" is-spaced p-4 has-background-dark" src={logo_blanco} alt="logo Andescalada" style={{ height: '200px' }} />
-                    </div>
+                      <br></br><a href="https://andescalada.org/img/logo_blanco.svg" target="_blank">SVG </a> | <a href="https://andescalada.org/img/logo_blanco.png" target="_blank">PNG </a>
+                  </div>
                     <div className="column">
                       <h4 className="title is-size-5 is-size-5-mobile is-spaced">  Version positiva </h4>
                       <img className=" is-spaced p-4" src={logo_negro} alt="logo Andescalada" style={{ height: '200px' }} />
-                    </div>
+                      <br></br><a href="https://andescalada.org/img/logo_negro.svg" target="_blank">SVG </a> | <a href="https://andescalada.org/img/logo_negro.png" target="_blank">PNG </a>
+                  </div>
                   </div>
                   <div className="columns">
                     <div className="column">
                       <h4 className="title is-size-5 is-size-5-mobile">Isotipo </h4>
                       <img className=" " src={logo_app} alt="logo Andescalada" style={{ height: '200px' }} />
-                    </div>
+                      <br></br><a href="https://andescalada.org/img/logo_app.svg" target="_blank">SVG </a> | <a href="https://andescalada.org/img/logo_app.png" target="_blank">PNG </a>
+                  </div>
                     <div className="column">
                       <h4 className="title is-size-5 is-size-5-mobile">Isotipo negro</h4>
                       <img className="p-6" src={isotipo_negro} alt="logo Andescalada" style={{ height: '200px' }} />
+                      <br></br><a href="https://andescalada.org/img/isotipo_negro.svg" target="_blank">SVG </a> | <a href="https://andescalada.org/img/isotipo_negro.png" target="_blank">PNG </a>
                     </div>
                     <div className="column">
                       <h4 className="title is-size-5 is-size-5-mobile">Isotipo morado</h4>
                       <img className="p-6" src={isotipo_morado} alt="logo Andescalada" style={{ height: '200px' }} />
+                      <br></br><a href="https://andescalada.org/img/isotipo_morado.svg" target="_blank">SVG </a> | <a href="https://andescalada.org/img/isotipo_morado.png" target="_blank">PNG </a>
                     </div>
                   </div>
                 </div>
@@ -95,7 +101,6 @@ export const BrandPageTemplate = ({
                 <h3 className="title is-size-4 is-size-4-mobile is-spaced">
                   Tipografía secundaria: Antonio
                 </h3>
-
 
                 <p className="title is-size-2 is-size-2-mobile secondary-font has-text-weight-bold">
                   ABCDEFGHIJKLMOPQRSTUVWXYZ <br></br> 0123456789
@@ -134,13 +139,13 @@ export const BrandPageTemplate = ({
                   </div>
               </section>
 
-              <section id="secondary-colors" className="pt-6 pb-6 mb-6">
+              <section id="secondary-colors" className="pt-6">
                 <h3 className="title is-size-4 is-size-4-mobile is-spaced">
                   Colores complementarios
                 </h3>
                   <div className="columns">
                     <div className="column">
-                      <div class="mustard-square">hola </div>
+                      <div class="mustard-square"></div>
                     </div>
                     <div className="column">
                       <p className="">
@@ -162,49 +167,49 @@ export const BrandPageTemplate = ({
                   </div>
               </section>
 
-              <section id="secondary-colors" className="pt-6 pb-6 mb-6">
-                <h2 className="title is-size-3 is-size-3-mobile is-spaced">
-                  <span class="underline--magical"> Accesibilidad</span>
-                </h2>
+              <section id="accesibility" className="pt-6 pb-6 mb-6">
                 <h3 className="title is-size-4 is-size-4-mobile is-spaced">
                   Accesibilidad
                 </h3>
-
+                <p className="mb-3">
+                  En lo que refiere a uso de color, en Andescalada buscamos un <a href=">https://webaim.org/articles/contrast/">nivel de conformidad AAA</a>. En la tabla anterior se muestran los colores y fuentes que pueden usarse para tener un contraste suficiente.<br></br>
+                  - AA: Contraste mínimo. Solo usar para texto grande, es decir, texto de al menos 18pt o 14pt <br></br>
+                  - AAA: Contraste mejorado.
+                </p>
                 <div className="table-container">
                   <table className="table is-bordered is-fullwidth">
-
                     <thead>
                       <tr>
                         <th> Nombre </th>
                         <th> Color original </th>
-                        <th> Versión oscura </th>
-                        <th> Versión clara </th>
+                        <th> Versión oscura <br></br> <span className="is-size-6 has-text-weight-light">Utilizar con fuente blanca</span></th>
+                        <th> Versión clara <br></br> <span className="is-size-6 has-text-weight-light">Utilizar con fuente negra</span></th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr>
                         <td> Morado </td>
-                        <td className="bg-purple"> <p className="has-text-white"> AAA </p> </td>
-                        <td className="bg-purple-dark"> <p className="has-text-white"> AAA </p> </td>
-                        <td className="bg-purple-light"> <p className="has-text-black"> AAA </p> </td>
+                        <td className="bg-purple"> <p className="has-text-white"> AAA <br></br>#413d76 </p> </td>
+                        <td className="bg-purple-dark"> <p className="has-text-white"> AAA <br></br>#413d76 </p> </td>
+                        <td className="bg-purple-light"> <p className="has-text-black"> AAA <br></br>#9087FF </p> </td>
                       </tr>
                       <tr>
                         <td> Naranjo </td>
-                        <td className="bg-orange"> <p className="has-text-white"> AAA </p> </td>
-                        <td className="bg-orange-dark"> <p className="has-text-white"> AAA </p> </td>
-                        <td className="bg-orange-light"> <p className="has-text-black"> AAA </p> </td>
+                        <td className="bg-orange"> <p className="has-text-black"> AA <br></br>#c75f3d </p> </td>
+                        <td className="bg-orange-dark"> <p className="has-text-white"> AAA <br></br>#8C432B </p> </td>
+                        <td className="bg-orange-light"> <p className="has-text-black"> AAA <br></br>#ED7149 </p> </td>
                       </tr>
                       <tr>
                         <td> Mostaza </td>
-                        <td className="bg-mustard"> <p className="has-text-white"> AAA </p> </td>
-                        <td className="bg-mustard-dark"> <p className="has-text-white"> AAA </p> </td>
-                        <td className="bg-mustard-light"> <p className="has-text-black"> AAA </p> </td>
+                        <td className="bg-mustard"> <p className="has-text-black"> AAA <br></br>#e7b061 </p> </td>
+                        <td className="bg-mustard-dark"> <p className="has-text-white"> AAA <br></br>#916F3D </p> </td>
+                        <td className="bg-mustard-light"> <p className="has-text-black"> AAA <br></br>#e7b061 </p> </td>
                       </tr>
                       <tr>
                         <td> Celeste </td>
-                        <td className="bg-cyan"> <p className="has-text-white"> AAA </p> </td>
-                        <td className="bg-cyan-dark"> <p className="has-text-white"> AAA </p> </td>
-                        <td className="bg-cyan-light"> <p className="has-text-black"> AAA </p> </td>
+                        <td className="bg-cyan"> <p className="has-text-black"> AAA <br></br>#8cbae7 </p> </td>
+                        <td className="bg-cyan-dark"> <p className="has-text-white"> AAA <br></br>#445B70 </p> </td>
+                        <td className="bg-cyan-light"> <p className="has-text-black"> AAA <br></br>#8cbae7 </p> </td>
                       </tr>
 
                     </tbody>
